3a4b93631b264439e60d3b5ce56f82cb1d9e55ca
wonkatin <j.drawers@gmail.com>
2019-01-30 21:08:03 -0500

    Created App JS & CSS (4th Commit)

---
+++ src/components/app/App.js
@@ -0,0 +1,19 @@
+import './App.css'
+import SearchBar from '././searchbar/Searchbar';
+import SearchResults from '././searchresults/SearchResults';
+import Playlist from '././playlist/Playlist';
+
+render() {
+  return (
+    <div>
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+      <div class="App">
+        <!-- Add a SearchBar component -->
+        <div class="App-playlist">
+          <!-- Add a SearchResults component -->
+          <!-- Add a Playlist component -->
+        </div>
+      </div>
+    </div>
+  )
+};
